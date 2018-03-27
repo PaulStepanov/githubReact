c6691a5e4f65779b6f237e427a46fcbaa0d6d914
PavelStep <pavelstseanau@gmail.com>
2018-03-27 15:33:08 +0300

    s

---
+++ src/scenes/Repo/scenes/Code/scenes/SourceViewer/scenes/Files/components/file/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {FILE_PROP_TYPE_SHAPE} from '../../constants';
+
+const index = ({isDirectory, name, lastCommit, lastCommitAgoText, onClick}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+index.propTypes = FILE_PROP_TYPE_SHAPE;
+
+export default index;
